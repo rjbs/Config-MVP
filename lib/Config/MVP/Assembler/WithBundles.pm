69bc9c19ed827bac4cb40861895118d13980ba14
Ricardo Signes <rjbs@cpan.org>
2009-10-26 12:39:43 -0400

    document bundles, remove dead method

--- lib/Config/MVP/Assembler/WithBundles.pm
+++ lib/Config/MVP/Assembler/WithBundles.pm
@@ -2,6 +2,29 @@ package Config::MVP::Assembler::WithBundles;
 use Moose::Role;
 # ABSTRACT: a role to make assemblers expand bundles
 
+=head1 DESCRIPTION
+
+Config::MVP::Assembler::WithBundles is a role to be composed into a
+Config::MVP::Assembler subclass.  It allows some sections of configuration to
+be treated as bundles.  When any section is ended, if that section represented
+a bundle, its bundle contents will be unrolled and will replace it in the
+sequence.
+
+A package is considered a bundle if the this returns a defined method:
+
+  my $method = $assembler->package_bundle_method($package);
+
+The default implementation looks for a method callde C<mvp_bundle_config>, but
+C<package_bundle_method> can be replaced to allow for other bundle-identifying
+information.
+
+Bundles are expanded by having their bundle method called.  The arguments
+passed to this method B<are likely to change>.  Currently, it's passed a
+hashref containing the bundle section's payload and a C<plugin_name> entry with
+the section name.
+
+=cut
+
 sub package_bundle_method {
   my ($self, $pkg) = @_;
   return unless $pkg->can('mvp_bundle_config');
@@ -45,21 +68,5 @@ after end_section => sub {
   }
 };
 
-sub expand_bundles {
-  my ($self, $plugins) = @_;
-
-  my @new_plugins;
-
-  for my $plugin (@$plugins) {
-    if ($self->package_is_bundle($plugin->[1])) {
-    } else {
-      push @new_plugins, $plugin;
-    }
-  }
-
-  @$plugins = @new_plugins;
-}
-
 no Moose;
 1;
-
